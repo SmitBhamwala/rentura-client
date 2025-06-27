4c9b7944ef63d08ee54a29af29e2f7149e06201f
Smit Bhamwala <smit.bhamwala@terumobct.com>
2025-06-27 15:20:32 +0530

    FiltersBar UI fix

--- src/app/(nondashboard)/search/FiltersBar.tsx
+++ src/app/(nondashboard)/search/FiltersBar.tsx
@@ -80,8 +80,8 @@ export default function FiltersBar() {
         <Button
           variant="outline"
           className={cn(
-            "gap-2 rounded-xl border-primary-400 hover:bg-primary-500 hover:text-primary-100",
-            isFiltersFullOpen && "bg-primary-700 text-primary-100"
+            "gap-2 rounded-xl border-primary-400 cursor-pointer",
+            isFiltersFullOpen && "bg-primary-700! text-primary-100!"
           )}
           onClick={() => dispatch(toggleFiltersFullOpen())}
         >
@@ -99,7 +99,7 @@ export default function FiltersBar() {
           />
           <Button
             // onClick={handleLocationSearch}
-            className={`rounded-r-xl rounded-l-none border-l-0 border-primary-400 shadow-none border hover:bg-primary-700 hover:text-primary-50`}
+            className={`rounded-r-xl rounded-l-none border-l-0 border-primary-400 shadow-none border hover:bg-primary-700 hover:text-primary-50 cursor-pointer`}
           >
             <Search className="w-4 h-4" />
           </Button>
@@ -114,15 +114,21 @@ export default function FiltersBar() {
               handleFilterChange("priceRange", value, true)
             }
           >
-            <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0">
+            <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0 cursor-pointer">
               <SelectValue>
                 {formatPriceValue(filters.priceRange[0], true)}
               </SelectValue>
             </SelectTrigger>
             <SelectContent className="bg-white">
-              <SelectItem value="any">Any Min Price</SelectItem>
+              <SelectItem value="any" className="cursor-pointer">
+                Any Min Price
+              </SelectItem>
               {[500, 1000, 1500, 2000, 3000, 5000, 10000].map((price) => (
-                <SelectItem key={price} value={price.toString()}>
+                <SelectItem
+                  key={price}
+                  value={price.toString()}
+                  className="cursor-pointer"
+                >
                   Rs {price / 1000}k+
                 </SelectItem>
               ))}
@@ -136,15 +142,21 @@ export default function FiltersBar() {
               handleFilterChange("priceRange", value, false)
             }
           >
-            <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0">
+            <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0 cursor-pointer">
               <SelectValue>
                 {formatPriceValue(filters.priceRange[1], false)}
               </SelectValue>
             </SelectTrigger>
             <SelectContent className="bg-white">
-              <SelectItem value="any">Any Max Price</SelectItem>
+              <SelectItem value="any" className="cursor-pointer">
+                Any Max Price
+              </SelectItem>
               {[1000, 2000, 3000, 5000, 10000].map((price) => (
-                <SelectItem key={price} value={price.toString()}>
+                <SelectItem
+                  key={price}
+                  value={price.toString()}
+                  className="cursor-pointer"
+                >
                   &lt; Rs {price / 1000}k
                 </SelectItem>
               ))}
@@ -159,15 +171,25 @@ export default function FiltersBar() {
             value={filters.beds}
             onValueChange={(value) => handleFilterChange("beds", value, null)}
           >
-            <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0">
+            <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0 cursor-pointer">
               <SelectValue placeholder="Beds" />
             </SelectTrigger>
             <SelectContent className="bg-white">
-              <SelectItem value="any">Any Beds</SelectItem>
-              <SelectItem value="1">1+ bed</SelectItem>
-              <SelectItem value="2">2+ beds</SelectItem>
-              <SelectItem value="3">3+ beds</SelectItem>
-              <SelectItem value="4">4+ beds</SelectItem>
+              <SelectItem value="any" className="cursor-pointer">
+                Any Beds
+              </SelectItem>
+              <SelectItem value="1" className="cursor-pointer">
+                1+ bed
+              </SelectItem>
+              <SelectItem value="2" className="cursor-pointer">
+                2+ beds
+              </SelectItem>
+              <SelectItem value="3" className="cursor-pointer">
+                3+ beds
+              </SelectItem>
+              <SelectItem value="4" className="cursor-pointer">
+                4+ beds
+              </SelectItem>
             </SelectContent>
           </Select>
 
@@ -176,14 +198,22 @@ export default function FiltersBar() {
             value={filters.baths}
             onValueChange={(value) => handleFilterChange("baths", value, null)}
           >
-            <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0">
+            <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0 cursor-pointer">
               <SelectValue placeholder="Baths" />
             </SelectTrigger>
             <SelectContent className="bg-white">
-              <SelectItem value="any">Any Baths</SelectItem>
-              <SelectItem value="1">1+ bath</SelectItem>
-              <SelectItem value="2">2+ baths</SelectItem>
-              <SelectItem value="3">3+ baths</SelectItem>
+              <SelectItem value="any" className="cursor-pointer">
+                Any Baths
+              </SelectItem>
+              <SelectItem value="1" className="cursor-pointer">
+                1+ bath
+              </SelectItem>
+              <SelectItem value="2" className="cursor-pointer">
+                2+ baths
+              </SelectItem>
+              <SelectItem value="3" className="cursor-pointer">
+                3+ baths
+              </SelectItem>
             </SelectContent>
           </Select>
         </div>
@@ -195,13 +225,15 @@ export default function FiltersBar() {
             handleFilterChange("propertyType", value, null)
           }
         >
-          <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0">
+          <SelectTrigger className="rounded-xl border-primary-400! focus-visible:ring-0 cursor-pointer">
             <SelectValue placeholder="Home Type" />
           </SelectTrigger>
           <SelectContent className="bg-white">
-            <SelectItem value="any">Any Property Type</SelectItem>
+            <SelectItem value="any" className="cursor-pointer">
+              Any Property Type
+            </SelectItem>
             {Object.entries(PropertyTypeIcons).map(([type, Icon]) => (
-              <SelectItem key={type} value={type}>
+              <SelectItem key={type} value={type} className="cursor-pointer">
                 <div className="flex items-center">
                   <Icon className="w-4 h-4 mr-2" />
                   <span>{type}</span>
