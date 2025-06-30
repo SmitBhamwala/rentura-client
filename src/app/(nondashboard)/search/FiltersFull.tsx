382ad4cf70d07cd1c599a3c2de05180b0a8c13c0
Smit Bhamwala <smit.bhamwala@terumobct.com>
2025-06-30 14:42:21 +0530

    position Fix for apply and reset buttons

--- src/app/(nondashboard)/search/FiltersFull.tsx
+++ src/app/(nondashboard)/search/FiltersFull.tsx
@@ -67,8 +67,8 @@ export default function FiltersFull() {
   }
 
   return (
-    <div className="bg-white rounded-lg px-4 h-full overflow-auto pb-10">
-      <div className="flex flex-col space-y-6">
+    <div className="bg-white rounded-lg px-4 h-full pb-10">
+      <div className="flex flex-col h-[70vh] overflow-y-scroll space-y-6 pr-4">
         {/* Location */}
         <div>
           <h4 className="font-bold mb-2">Location</h4>
@@ -270,8 +270,11 @@ export default function FiltersFull() {
           />
         </div>
 
-        {/* Apply and Reset buttons */}
-        <div className="flex gap-4 mt-6">
+        
+      </div>
+
+      {/* Apply and Reset buttons */}
+        <div className="flex gap-4 mt-6 sticky bottom-0">
           <Button
             onClick={handleSubmit}
             className="flex-1 bg-primary-700 text-white rounded-xl cursor-pointer">
@@ -284,7 +287,6 @@ export default function FiltersFull() {
             Reset Filters
           </Button>
         </div>
-      </div>
     </div>
   );
 }
